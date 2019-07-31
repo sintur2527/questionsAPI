7ebd3371858f9a73ac12e464014dfce622d3d026
Soumith Inturi <sintur2527@gmail.com>
2019-07-31 14:55:45 -0400

    Added queries

--- controllers/index.js
+++ controllers/index.js
@@ -13,4 +13,28 @@ module.exports = {
         });
     },
   },
+  answers: {
+    get: (req, res) => {
+      models.answers
+        .get()
+        .then(data => {
+          res.status(200).send(data);
+        })
+        .catch(err => {
+          res.status(500).send(err);
+        });
+    },
+  },
+  photos: {
+    get: (req, res) => {
+      models.photos
+        .get()
+        .then(data => {
+          res.status(200).send(data);
+        })
+        .catch(err => {
+          res.status(500).send(err);
+        });
+    },
+  },
 };
--- models/index.js
+++ models/index.js
@@ -6,4 +6,14 @@ module.exports = {
       return db.any('SELECT * FROM questions WHERE id < 6');
     },
   },
+  answers: {
+    get: () => {
+      return db.any('SELECT * FROM answers WHERE id < 6');
+    },
+  },
+  photos: {
+    get: () => {
+      return db.any('SELECT * FROM photos WHERE id < 6');
+    },
+  },
 };
