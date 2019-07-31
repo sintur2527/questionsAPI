cb83868caf71e0993154fdcc7de5d94b8b7eaeca
Soumith Inturi <sintur2527@gmail.com>
2019-07-31 10:40:45 -0400

    Created express server

---
+++ db/index.js
@@ -0,0 +1,2 @@
+const pgp = require('pg-promise');
+const db = pgp('postgres://soumithinturi:zxcv@localhost:5432/questions');
---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
