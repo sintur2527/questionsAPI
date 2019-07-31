9e91d07613c39b92ad8ddcd28315d56a9457dcbd
Soumith Inturi <sintur2527@gmail.com>
2019-07-31 10:20:53 -0400

    Loading answers and photos from csv

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE questions;
 \c questions;
 
 CREATE TABLE questions (
-  id SERIAL PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   product_id INTEGER, 
   body VARCHAR NOT NULL,
   date_written TIMESTAMP NOT NULL,
@@ -17,24 +17,23 @@ CREATE TABLE questions (
 COPY questions FROM '/Users/soumithinturi/Documents/hack-reactor-nyc23/hammer-questions/data/questions.csv' DELIMITERS ',' CSV HEADER;
 
 CREATE TABLE answers (
-  id SERIAL PRIMARY KEY,
-  question_id INTEGER, 
+  id INTEGER PRIMARY KEY NOT NULL,
+  question_id INTEGER , 
   body VARCHAR NOT NULL,
   date_written TIMESTAMP NOT NULL,
   answerer_name VARCHAR (50) NOT NULL,
   answerer_email VARCHAR (100) NOT NULL,
   reported BOOLEAN,
-  helpful INTEGER,
-  FOREIGN KEY (question_id) REFERENCES questions (id)
+  helpful INTEGER  
 );
 
 COPY answers FROM '/Users/soumithinturi/Documents/hack-reactor-nyc23/hammer-questions/data/answers.csv' DELIMITERS ',' CSV HEADER;
 
 CREATE TABLE photos (
-  id SERIAL PRIMARY KEY,
+  id INTEGER NOT NULL,
   answer_id INTEGER,
   url VARCHAR,
-  FOREIGN KEY (answer_id) REFERENCES answers (id)
+  PRIMARY KEY (id, answer_id)
 );
 
 COPY photos FROM '/Users/soumithinturi/Documents/hack-reactor-nyc23/hammer-questions/data/answers_photos.csv' DELIMITERS ',' CSV HEADER;
