a8f6d9f3bf617abef5e8741768bec2cf4027b735
Soumith Inturi <sintur2527@gmail.com>
2019-08-06 19:06:57 -0400

    Renamed columns in schema tables

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -40,6 +40,14 @@ SELECT setval('questions_id_seq', (SELECT count(*) FROM questions), true);
 SELECT setval('answers_id_seq', (SELECT count(*) FROM answers), true);
 SELECT setval('photos_id_seq', (SELECT count(*) FROM photos), true);
 
+ALTER TABLE questions RENAME COLUMN id TO question_id;
+ALTER TABLE questions RENAME COLUMN body TO question_body;
+ALTER TABLE questions RENAME COLUMN date_written TO question_date;
+ALTER TABLE questions RENAME COLUMN helpful TO question_helpfulness;
+
+ALTER TABLE answers RENAME COLUMN date_written TO date;
+ALTER TABLE answers RENAME COLUMN helpful TO helpfulness;
+
 CREATE INDEX question_id_idx ON answers (question_id);
 CREATE INDEX answer_id_idx ON photos (answer_id);
 
